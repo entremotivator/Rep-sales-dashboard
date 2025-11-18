a375b2ba3893b60ca86e4742b585f29c1b922617
entremotivator <104661532+entremotivator@users.noreply.github.com>
2025-11-17 20:15:21 -0500

    Update advanced_sales_dashboard.py

--- advanced_sales_dashboard.py
+++ advanced_sales_dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+import plotly.express
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import json
